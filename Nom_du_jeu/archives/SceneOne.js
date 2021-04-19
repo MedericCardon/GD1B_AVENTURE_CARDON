e10e960d3446e56896b72f9f7ebc0d28ed0344c9
Médé <medericcardon@gmail.com>
2021-04-19 10:11:14 +0200

    scene_3 + modif drop

---
+++ Nom_du_jeu/archives/SceneOne.js
@@ -0,0 +1,170 @@
+var player;
+var cursors;
+var passage_bas;
+var blockCentral;
+var blockCentral_2;
+var arbre;
+
+var compteur = 150; // par défaut: 150 //
+var invincible = false;
+var playerPdv = 5;
+var cle;
+var scoreCle = 0;
+var scoreGateau = 0;
+var scoreBonbon = 0;
+var texte_cle;
+var texte_bonbon;
+var texte_gateau;
+var gateau;
+var bonbon;
+
+var visibleGateau = false;
+var visibleBonbon = false;
+
+var keyZ;
+var keyQ;
+var keyS;
+var keyD;
+var keyE;
+var spaceBar;
+
+var gamepad;
+var paddle;
+var padConnected;
+var pad;
+
+class Scene_01 extends Phaser.Scene{
+    constructor(){
+        super("Scene_01");
+    }
+    init(data){
+    }
+    preload(){
+
+        // ----- Decors ----- //
+
+        this.load.image('passage_bas','assets/01_decors/block_decors-assets/passage_zone_01.png')
+        this.load.image('scene_01','assets/02_spriteSheet_personnage/scene_01.png')
+        this.load.image('player','assets/02_spriteSheet_personnage/player.png');
+        this.load.image('arbre', 'assets/01_decors/block_decors-assets/arbre.png')
+        this.load.image('blockCentral', 'assets/01_decors/block_decors-assets/block_central.png')
+        this.load.image('blockCentral_2', 'assets/01_decors/block_decors-assets/block_central_2.png')
+
+        // ----- Items & HUD ----- //
+
+        this.load.image('cle','assets/04_items/Item_collectibles-assets/cle.png')
+        this.load.image('gateau','assets/04_items/Item_collectibles-assets/gateau.png')
+        this.load.image('bonbon','assets/04_items/Item_collectibles-assets/bonbon.png')
+        this.load.image('HUD','assets/04_items/Item_collectibles-assets/HUD.png')
+    }
+    create(){
+
+        
+        
+        this.add.image(0,0,'scene_01').setOrigin(0).setScrollFactor(0);
+        passage_bas = this.physics.add.sprite(580, 700,'passage_bas');
+        
+
+        blockCentral = this.physics.add.staticGroup(); // Le personnage passe derrière //
+        blockCentral_2 = this.physics.add.staticGroup(); // Le personnage passe devant //
+        arbre = this.physics.add.staticGroup(); // arbre element de décor // 
+        
+
+       blockCentral.create(410,250,'blockCentral').setOrigin(0).setSize(250,20).setOffset(150,150);
+
+        // ----- Player ----- //
+
+        player = this.physics.add.sprite(300, 300, 'player');
+        player.setCollideWorldBounds(true);
+
+        this.add.image(0,0,'HUD').setOrigin(0);
+        
+       // ----- Arbres ----- //
+
+       arbre.create(460,40,'arbre').setOrigin(0);
+       arbre.create(800,50,'arbre').setOrigin(0);
+
+       
+       blockCentral_2.create(420,250,'blockCentral_2').setOrigin(0).setSize(0,0).setOffset(150,150);
+
+        this.physics.add.overlap(player,passage_bas, changementZone, null, this);
+        this.physics.add.collider(player,blockCentral);
+
+        // ----- Pad + touches clavier ----- //
+
+        cursors = this.input.keyboard.createCursorKeys();
+        keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+        keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+        spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        keyZ.reset();
+        keyQ.reset();
+        keyS.reset();
+        keyD.reset();
+
+
+        this.input.gamepad.once('connected', function (pad, button, index) {
+            paddle = pad;
+            padConnected = true;
+        }); 
+
+        // ----- score texte ----- //
+
+        texte_cle = this.add.text(80, 20, '0', { font: '20px Georgia', fill: '#f0acdc' });
+        texte_gateau = this.add.text(160,20, '0',{font: '20px Georgia', fill: '#f0acdc' });
+        texte_bonbon = this.add.text(230,19, '0',{font: '20px Georgia', fill: '#f0acdc' });
+        
+        function changementZone(){
+            this.scene.start("Scene_02");
+            player.setX(100);
+            player.setY(100);
+            console.log("changement");
+        }
+    }
+    
+    update(){
+
+        // ----- controles clavier ----- //
+        if (keyD.isDown){
+            player.setVelocityX(200);
+            player.setFlipX(false);
+        }
+        else if (keyQ.isDown){
+            player.setVelocityX(-200);
+            player.setFlipX(true);
+        }
+        else if (keyD.isUp && keyQ.isUp){
+            player.setVelocityX(0);
+        }
+        if (keyZ.isDown){
+            player.setVelocityY(-200);
+        }
+        else if (keyS.isDown){
+            player.setVelocityY(200);
+        }
+        else if (keyZ.isUp && keyS.isUp){
+            player.setVelocityY(0);
+        }
+        // ----- controles manette ----- //
+
+        if (padConnected) {
+
+            if(paddle.right){ 
+                player.setVelocityX(200);
+            }
+            if(paddle.left){ 
+                player.setVelocityX(-200);
+            }
+            if(paddle.up){ 
+                player.setVelocityY(-200);
+            }
+            if(paddle.down){
+                player.setVelocityY(200);
+            }
+        }
+            
+    }
+}
---
+++ Nom_du_jeu/travail/Scene_03.js
@@ -0,0 +1,285 @@
+var player;
+var cursors;
+var block_central_devant;
+var block_central_deriere;
+var block_bas_s3;
+var passage_s4;
+var ennemi_cerveau1;
+var ennemi_cerveau2;
+var compteur = 150; // par défaut: 150 //
+var invincible = false;
+var playerPdv = 5;
+var cle;
+var scoreCle = 0;
+var scoreGateau = 0;
+var scoreBonbon = 0;
+var texte_cle;
+var texte_bonbon;
+var texte_gateau;
+var gateau1;
+var bonbon1;
+var gateau2;
+var bonbon2;
+var visibleGateau1 = false;
+var visibleBonbon1 = false;
+
+var visibleGateau2 = false;
+var visibleBonbon2 = false;
+
+var keyZ;
+var keyQ;
+var keyS;
+var keyD;
+var keyE;
+var spaceBar;
+
+var gamepad;
+var paddle;
+var padConnected;
+var pad;
+
+
+
+class Scene_03 extends Phaser.Scene{
+    constructor(){
+        super("Scene_03");
+    }
+    init(data){
+    }
+    preload(){
+
+        this.load.image('player','assets/02_spriteSheet_personnage/player.png');
+        this.load.image('cle','assets/04_items/Item_collectibles-assets/cle.png')
+        this.load.image('gateau','assets/04_items/Item_collectibles-assets/gateau.png')
+        this.load.image('bonbon','assets/04_items/Item_collectibles-assets/bonbon.png')
+        this.load.image('HUD','assets/04_items/Item_collectibles-assets/HUD.png')
+
+        this.load.image('background_s3','assets/00_background_scenes/background_scene_03-assets/background.png')
+        this.load.image('arbres','assets/00_background_scenes/background_scene_03-assets/arbres_contour.png')
+        this.load.image('block_devant','assets/00_background_scenes/background_scene_03-assets/block_central_deriere.png')
+        this.load.image('block_arriere','assets/00_background_scenes/background_scene_03-assets/block_central_devant.png')
+        this.load.image('block_bas','assets/00_background_scenes/background_scene_03-assets/block_bas_droit.png')
+        this.load.image('passageHaut_s3', 'assets/01_decors/block_decors-assets/passage_zone_01.png');
+
+        this.load.spritesheet('ennemi_cerveau', 'assets/03_spriteSheet_monstre_cerveau/spriteSheet_monstre_cerveau-assets/spriteSheet_monstre_cerveau.png', { frameWidth: 155.75, frameHeight: 121 });
+    }
+
+    create(){
+
+        this.add.image(0,0,'background_s3').setOrigin(0).setScrollFactor(0);
+       
+        block_central_deriere = this.physics.add.staticGroup();
+        block_central_devant = this.physics.add.staticGroup();
+        block_bas_s3 = this.physics.add.staticGroup();
+        passage_s4 = this.physics.add.staticGroup();
+        
+
+        block_central_devant.create(815,275,'block_devant');
+
+
+        ennemi_cerveau1 = this.physics.add.sprite(400,300, 'ennemi_cerveau');
+        ennemi_cerveau1.body.setAllowGravity(false); // pas de gravité pour les ennemis //
+        ennemi_cerveau1.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'move_ennemi_cerveau',
+            frames: this.anims.generateFrameNumbers('ennemi_cerveau', {start:0, end : 3}),
+            frameRate : 7,
+            repeat: -1
+        });
+
+        this.tweens.add({
+            targets: ennemi_cerveau1,
+            props: {
+                x: { value: 600, duration: 2000, flipX: true },
+                y: { value: 200, duration: 3000},
+            },
+            yoyo: true,
+            repeat: -1
+        });
+
+        
+
+        ennemi_cerveau2 = this.physics.add.sprite(900,500, 'ennemi_cerveau');
+        ennemi_cerveau2.body.setAllowGravity(false); // pas de gravité pour les ennemis //
+        ennemi_cerveau2.setCollideWorldBounds(true);
+
+        this.tweens.add({
+            targets: ennemi_cerveau2,
+            props: {
+                x: { value: 600, duration: 2000, flipX: true },
+                y: { value: 450, duration: 3000},
+            },
+            yoyo: true,
+            repeat: -1
+        });
+
+        player = this.physics.add.sprite(200, 200, 'player');
+        player.setCollideWorldBounds(true);
+        player.setVelocity(0);
+
+        block_central_deriere.create(815,237,'block_arriere');
+        passage_s4.create(650,0,'passageHaut_s3');
+        block_bas_s3.create(1155,450,'block_bas');
+
+        this.add.image(0,0,'arbres').setOrigin(0).setScrollFactor(0);
+        
+        this.add.image(0,0,'HUD').setOrigin(0);
+
+        gateau1 = this.physics.add.sprite(ennemi_cerveau1.x,ennemi_cerveau1.y,'gateau').setAlpha(0);
+        bonbon1 = this.physics.add.sprite(ennemi_cerveau1.x,ennemi_cerveau1.y,'bonbon').setAlpha(0);
+
+        gateau2 = this.physics.add.sprite(ennemi_cerveau2.x,ennemi_cerveau2.y,'gateau').setAlpha(0);
+        bonbon2 = this.physics.add.sprite(ennemi_cerveau2.x,ennemi_cerveau2.y,'bonbon').setAlpha(0);
+
+        // ----- Pad + touches clavier ----- //
+
+        cursors = this.input.keyboard.createCursorKeys();
+        keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+        keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+        spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        keyZ.reset();
+        keyQ.reset();
+        keyS.reset();
+        keyD.reset();
+
+        this.input.gamepad.once('connected', function (pad) {
+            paddle = pad;
+            padConnected = true;
+        });
+
+        // ----- Score texte ----- //
+        texte_cle = this.add.text(80, 20, scoreCle, { font: '20px Georgia', fill: '#f0acdc' });
+        texte_gateau = this.add.text(160,20, scoreGateau,{font: '20px Georgia', fill: '#f0acdc' });
+        texte_bonbon = this.add.text(230,19, scoreBonbon,{font: '20px Georgia', fill: '#f0acdc' });
+
+        this.physics.add.overlap(player,ennemi_cerveau1,killEnnemi1,null,this);
+        this.physics.add.overlap(player,ennemi_cerveau2,killEnnemi2,null,this);
+        this.physics.add.overlap(player,cle,dropCle,null,this);
+        this.physics.add.overlap(player,bonbon1,dropBonbonS3,null,this);
+        this.physics.add.overlap(player,gateau1,dropGateauS3,null,this);
+        this.physics.add.overlap(player,bonbon2,dropBonbonS3,null,this);
+        this.physics.add.overlap(player,gateau2,dropGateauS3,null,this);
+    }
+
+    update(){
+
+        // ----- Compteurs ----- //
+
+        if(invincible == true){ // relance du compteur d'invulné player //
+            compteur-- ;
+            if(compteur == 0){
+                compteur = 150;
+                invincible = false ;
+            }
+        }
+
+        if (keyD.isDown){
+            player.setVelocityX(200);
+            ennemi_cerveau1.anims.play('move_ennemi_cerveau');
+            ennemi_cerveau2.anims.play('move_ennemi_cerveau');
+            player.setFlipX(false);
+        }
+        else if (keyQ.isDown){
+            player.setVelocityX(-200);
+            player.setFlipX(true);
+        }
+        else if (keyD.isUp && keyQ.isUp){
+            player.setVelocityX(0);
+        }
+        if (keyZ.isDown){
+            player.setVelocityY(-200);
+        }
+        else if (keyS.isDown){
+            player.setVelocityY(200);
+        }
+        else if (keyZ.isUp && keyS.isUp){
+            player.setVelocityY(0);
+        }
+
+        if (padConnected) {
+
+            if(paddle.right){ 
+                player.setVelocityX(200);
+            }
+            if(paddle.left){ 
+                player.setVelocityX(-200);
+            }
+            if(paddle.up){ 
+                player.setVelocityY(-200);
+            }
+            if(paddle.down){
+                player.setVelocityY(200);
+            }
+            
+        }
+
+    }
+
+}
+
+function hitEnnemi(){
+    if(invincible == false){
+        invincible = true ;
+        playerPdv -= 1;
+    }
+}
+function killEnnemi1(){
+    ennemi_cerveau1.destroy();
+    gateau1.setAlpha(1);
+    bonbon1.setAlpha(1);
+    gateau1.setX(ennemi_cerveau.x);
+    gateau1.setY(ennemi_cerveau.y);
+    visibleGateau1 = true;
+    bonbon1.setX(ennemi_cerveau1.x + 30);
+    bonbon1.setY(ennemi_cerveau1.y + 20);
+    visibleBonbon1 = true;
+}
+
+function killEnnemi2(){
+    ennemi_cerveau2.destroy();
+    gateau2.setAlpha(1);
+    bonbon2.setAlpha(1);
+    gateau2.setX(ennemi_cerveau2.x);
+    gateau2.setY(ennemi_cerveau2.y);
+    visibleGateau2 = true;
+    bonbon2.setX(ennemi_cerveau2.x + 30);
+    bonbon2.setY(ennemi_cerveau2.y + 20);
+    visibleBonbon2 = true;
+    
+}
+
+function dropCleS3(){
+    scoreCle += 1;
+    cle.destroy();
+    texte_cle.setText(scoreCle);
+}
+
+function dropBonbonS3(){
+    scoreBonbon +=7;
+    if(visibleBonbon1 == true){
+        bonbon1.destroy(true,true);
+        texte_bonbon.setText(scoreBonbon);
+    }
+    if(visibleBonbon2 == true){
+        bonbon2.destroy(true,true);
+        texte_bonbon.setText(scoreBonbon);
+    }
+}
+
+function dropGateauS3(){
+    scoreGateau +=1;
+    if(visibleGateau == true){
+        gateau.destroy(true,true);
+        texte_gateau.setText(scoreGateau);
+    }
+    if(visibleGateau2 == true){
+
+        gateau2.destroy(true,true);
+        texte_gateau.setText(scoreGateau);
+    }
+}
